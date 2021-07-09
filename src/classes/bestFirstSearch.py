0d88066371258a0b7e3425bae2f28b4a6fa6fae2
Gabriel <gabrielvbmarin21@gmail.com>
2021-07-08 21:04:30 -0300

    V1.1

--- src/classes/bestFirstSearch.py
+++ src/classes/bestFirstSearch.py
@@ -17,7 +17,7 @@ class BestFirstSearch():
             
             # Ordena os vizinhos mais próximos do destino
             for neighbour in self.graph.edges[vertex]:
-                distance = ((((self.graph.nodes[neighbour].x - self.graph.nodes[rootNodeKey].x)**2) + ((self.graph.nodes[neighbour].y - self.graph.nodes[rootNodeKey].y)**2))**0.5)
+                distance = ((((self.graph.nodes[destNodeKey].x - self.graph.nodes[rootNodeKey].x)**2) + ((self.graph.nodes[destNodeKey].y - self.graph.nodes[rootNodeKey].y)**2))**0.5)
                 orderedEdges.append((neighbour, distance))
 
             orderedEdges.sort(key=lambda tup: tup[1])
